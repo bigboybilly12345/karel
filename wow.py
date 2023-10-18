c71ed6e740b3f54fee7701ee005737de7e9df35d
Jesus <jesusjunior225@gmail.com>
2023-10-18 17:17:29 -0500

    added stuff

--- wow.py
+++ wow.py
@@ -1,36 +1,42 @@
-from stanfordkarel import move, turn_left, put_beeper, run_karel_program
+from stanfordkarel import move, turn_left, put_beeper, front_is_blocked, run_karel_program
 
-def main():
-    pass
-    pick_beeper()
-    row_2()
+def move_if_blocked_turn_left():
+    if front_is_blocked():
+        turn_left()
+    else:
+        move()
+
+def test_2():
+    turn_left()
+    sweep()
+    change_right_top()
+    sweep()
+    change_right_bottom()
+
+def test():
+    test_2()
 
 
-if __name__ == "__main__":
-    run_karel_program("challenge-v-wall3")
-
-
-
-
-
-
-
+def change_right_bottom():
+    turn_left()
+    move_if_blocked_turn_left()
+    turn_left()
 def sweep():
     while front_is_clear():
-        move()
+        move_if_blocked_turn_left()
         if beepers_present():
             pick_beeper()
 
 def change_left():
     turn_left()
-    move()
+    move_if_blocked_turn_left()
     turn_left()
     if beepers_present():
         pick_beeper()
 
-def change_right():
+def change_right_top():
     turn_right()
-    move()
+    move_if_blocked_turn_left()
     turn_right()
     if beepers_present():
         pick_beeper()
@@ -44,45 +50,45 @@ def row_2():
         turn_left()
         sweep()
         turn_right()
-        move()
+        move_if_blocked_turn_left()
         turn_right()
         sweep()
         turn_left()
-        move()
+        move_if_blocked_turn_left()
         turn_left()
         sweep()
         turn_left()
         turn_left()
         sweep()
         turn_left()
-        move()
-        turn_left()
-        sweep()
-        turn_right()
-        move()
-        turn_right()
-        sweep()
-        turn_left()
-        move()
+        move_if_blocked_turn_left()
         turn_left()
         sweep()
         turn_right()
-        move()
+        move_if_blocked_turn_left()
         turn_right()
         sweep()
         turn_left()
-        move()
-        turn_left()
-        sweep()
-        turn_left()
-        turn_left()
-        sweep()
-        turn_left()
-        move()
+        move_if_blocked_turn_left()
         turn_left()
         sweep()
         turn_right()
-        move()
+        move_if_blocked_turn_left()
+        turn_right()
+        sweep()
+        turn_left()
+        move_if_blocked_turn_left()
+        turn_left()
+        sweep()
+        turn_left()
+        turn_left()
+        sweep()
+        turn_left()
+        move_if_blocked_turn_left()
+        turn_left()
+        sweep()
+        turn_right()
+        move_if_blocked_turn_left()
         turn_right()
         sweep()
 
@@ -108,3 +114,10 @@ def row():
         sweep()
         change_left()
         sweep()
+
+def main():
+    pick_beeper()
+    test()
+
+if __name__ == "__main__":
+    run_karel_program("challenge-v-wall3")
